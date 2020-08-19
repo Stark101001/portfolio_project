824f30f0a68cae9685c5323ae9163a8d725b1d3a
Pawan Kumar <starkdsouza786@gmail.com>
2020-08-19 10:27:11 +0530

    added some files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class blog(models.Model):
+    title = models.CharField(max_length=250)
+    publish_date = models.DateTimeField()
+    image = models.ImageField(upload_to="images/")
+    body = models.TextField()
